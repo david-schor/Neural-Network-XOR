2b0a54edc44b0c4912a9c9077742405b5d93de0f
David Schor <david.schor2000@gmail.com>
2017-07-10 18:47:40 +0200

    Add creation of network

--- src/com/neuralnet/Main.java
+++ src/com/neuralnet/Main.java
@@ -2,7 +2,34 @@ package com.neuralnet;
 
 public class Main {
 
+    private static double MINWEIGHT = 0.1;
+    private static double MAXWEIGHT = 2.0;
+
     public static void main(String[] args) {
-	// write your code here
+
+        Layer inputLayer = new Layer(null);
+        Neuron inputOne = new Neuron();
+        Neuron inputTwo = new Neuron();
+
+        inputOne.setInput(1);
+        inputTwo.setInput(1);
+
+        inputLayer.addNeuron(inputOne);
+        inputLayer.addNeuron(inputTwo);
+
+        Layer hiddenLayer = new Layer(inputLayer);
+        Neuron hiddenOne = new Neuron();
+        Neuron hiddenTwo = new Neuron();
+        Neuron hiddenThree = new Neuron();
+
+        hiddenLayer.addNeuron(hiddenOne, randomWeights());
+        hiddenLayer.addNeuron(hiddenTwo, randomWeights());
+        hiddenLayer.addNeuron(hiddenThree, randomWeights());
+
+    }
+
+    private static double[] randomWeights(){
+
+        return new double[]{MINWEIGHT + Math.random( ) * MAXWEIGHT, MINWEIGHT + Math.random( ) * MAXWEIGHT};
     }
 }
