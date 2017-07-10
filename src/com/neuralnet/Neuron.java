10e60adabf8ba03c468c7e7d255d91ddece74866
David Schor <david.schor2000@gmail.com>
2017-07-10 18:18:17 +0200

    Add Neuron.java

---
+++ src/com/neuralnet/Neuron.java
@@ -0,0 +1,15 @@
+package com.neuralnet;
+
+public class Neuron {
+
+    private double input;
+
+    public void setInput(double input){
+        this.input = input;
+    }
+
+    public double getInput(){
+        return input;
+    }
+
+}
